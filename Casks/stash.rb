11c24a650be0ea36cba7a00e6c885fe156084c54
hex <hex@users.noreply.github.com>
2026-02-08 17:15:37 +0200

    Add Stash cask formula
    Homebrew Cask for Stash, a lightweight macOS clipboard history manager.
    Install with: brew install --cask hex/tap/stash

---
+++ Casks/stash.rb
@@ -0,0 +1,18 @@
+cask "stash" do
+  version "2026.2.0"
+  sha256 "df39e697421f2e59932db863d6e2049bd8ba87af59adecb07221c75f0e8c5646"
+
+  url "https://github.com/hex/Stash/releases/download/v#{version}/Stash-#{version}.zip"
+  name "Stash"
+  desc "Lightweight macOS clipboard history manager"
+  homepage "https://github.com/hex/Stash"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Stash.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.hexul.Stash",
+    "~/Library/Preferences/com.hexul.Stash.plist",
+  ]
+end
