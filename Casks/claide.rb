c2c277994a6ae464f7a0ae65086294444b00ecac
Alexandru Geana <alex.geana@erepubliklabs.com>
2026-02-16 10:37:16 +0200

    Update claide to 2026.2.2

--- Casks/claide.rb
+++ Casks/claide.rb
@@ -2,8 +2,8 @@
 # ABOUTME: Auto-updated by CI on each GitHub Release.
 
 cask "claide" do
-  version "2026.2.1"
-  sha256 "2f71bcf135ba1674d28a8df9f90e4da21cd6d3dd3ef70be88b1d3c461cba7109"
+  version "2026.2.2"
+  sha256 "b06c7b4bea39982e1cdeb55ce9235f4d20b3d280a14c3b3325575ad3b1284968"
 
   url "https://github.com/hex/Claide/releases/download/v#{version}/Claide-#{version}.dmg"
   name "Claide"
