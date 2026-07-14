0ca1fb7ff968591229cbc33ab2243443ce77b681
Alexandru Geana <alex.geana@erepubliklabs.com>
2026-07-14 17:55:54 +0300

    snip: add cask (2026.7.0)

---
+++ Casks/snip.rb
@@ -0,0 +1,18 @@
+cask "snip" do
+  version "2026.7.0"
+  sha256 "2e55a80feafb659f3f456e8e5f63e654cb464af2c01e8d24511eba84a321e0db"
+
+  url "https://github.com/hex/Snip/releases/download/v#{version}/Snip-#{version}.zip"
+  name "Snip"
+  desc "Radial snippet menu for the macOS menu bar"
+  homepage "https://snip.hexul.com"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Snip.app"
+
+  zap trash: [
+    "~/Library/Application Support/Snip",
+    "~/Library/Preferences/ai.symbiotica.Snip.plist",
+  ]
+end
