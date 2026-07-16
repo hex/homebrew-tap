ba47a139b69e41ef6e2dd352a507562a1fdfa7b3
Alexandru Geana <alex.geana@erepubliklabs.com>
2026-07-16 12:19:11 +0300

    snip: 2026.7.1

--- Casks/snip.rb
+++ Casks/snip.rb
@@ -1,6 +1,6 @@
 cask "snip" do
-  version "2026.7.0"
-  sha256 "2e55a80feafb659f3f456e8e5f63e654cb464af2c01e8d24511eba84a321e0db"
+  version "2026.7.1"
+  sha256 "6034f484d9ca251358484390be95cb88b31e75eeba53fddf55718a834e1b6687"
 
   url "https://github.com/hex/Snip/releases/download/v#{version}/Snip-#{version}.zip",
       verified: "github.com/hex/Snip/"
