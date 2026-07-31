ac6995a7838e6b6369597c51a2c52269bc35cbbb
Alexandru Geana <alex.geana@erepubliklabs.com>
2026-07-31 12:02:06 +0200

    stash: 2026.7.1

--- Casks/stash.rb
+++ Casks/stash.rb
@@ -1,6 +1,6 @@
 cask "stash" do
-  version "2026.7.0"
-  sha256 "91bf2889fa7d774f973594a29cd354d6c6854bf89fd493dfa5e45daba08f5af4"
+  version "2026.7.1"
+  sha256 "56418b189384ecb6fd077320fe8334275e9c1b7af95df7f539d520e265d01197"
 
   url "https://github.com/hex/Stash/releases/download/v#{version}/Stash-#{version}.zip"
   name "Stash"
